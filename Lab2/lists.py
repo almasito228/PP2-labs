c011ab1d2aa8a3948a0ebff9212301fa7a436133
Almas <tursynali.10do@gmail.ru>
2025-01-28 13:49:31 +0500

    Examples

---
+++ Lab2/lists.py
@@ -0,0 +1,13 @@
+#1 
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+#2 Print the number of items in the list:
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+#3 Using the list() constructor to make a List:
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+#4 Print the second item of the list:
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+#5 
